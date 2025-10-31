f0ec33c6f5668533df486fe8b73dd7e864993759
Ranna272002 <ranna@eg.dk>
2025-10-31 13:36:40 +0530

    Generic class

---
+++ StudentManagement/Services/ICourseService.cs
@@ -0,0 +1,13 @@
+﻿using StudentManagement.Models;
+
+namespace StudentManagement.Services
+{
+    public interface ICourseService
+    {
+        Task<IEnumerable<Course>> GetAllCoursesAsync();
+        Task<Course?> GetCourseByIdAsync(int id);
+        Task<Course> CreateCourseAsync(Course course);
+        Task<bool> UpdateCourseAsync(int id, Course course);
+        Task<bool> DeleteCourseAsync(int id);
+    }
+}
