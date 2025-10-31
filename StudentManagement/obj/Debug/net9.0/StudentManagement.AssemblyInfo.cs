6a36f0ac2359a58a6d057949c7698034bedf2184
Ranna272002 <ranna@eg.dk>
2025-10-31 13:43:28 +0530

    file change

--- StudentManagement/obj/Debug/net9.0/StudentManagement.AssemblyInfo.cs
+++ StudentManagement/obj/Debug/net9.0/StudentManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+708f19c97af51f794664f1d0faee40d0d705038b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a91e10179364bd1c2695749c3d8d3b4a5ff3452")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
